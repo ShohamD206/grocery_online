e2b3d7f7ea969062934d1113859af7a2076e4e2e
shoham <shohamD.206@gmail.com>
2022-08-15 21:33:45 +0300

    added contact controller & model class, connect "home" and "contact" links

--- src/main/java/com/marktProject/GroceryMarket/controller/HomeController.java
+++ src/main/java/com/marktProject/GroceryMarket/controller/HomeController.java
@@ -1,14 +1,11 @@
 package com.marktProject.GroceryMarket.controller;
 
-import com.marktProject.GroceryMarket.model.Contact;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class IndexController {
+public class HomeController {
 
     @GetMapping("/home")
     public String displayHomePage(Model model) {
