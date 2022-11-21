816a9c0f39da1e94cf44e5d5b3edae1c4a854220
shoham <shohamD.206@gmail.com>
2022-11-21 18:27:42 +0200

    customized annotation 'FieldsMatch' & 'SecuredPassword'

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `user` (`name`, `email`, `password`, `mobile_num`)
-    VALUES ('ShohamD','shohamD.206@groceryAdmin.com', 0000 , '0545700670');
-
-INSERT INTO `user` (`name`, `email`, `password`, `mobile_num`)
-    VALUES ('NaorC','naorC@groceryAdmin.com', 1111 , '0545200650');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,13 +16,45 @@ CREATE TABLE IF NOT EXISTS `inquiry` (
     `updated_by` varchar(50) DEFAULT NULL
 );
 
+CREATE TABLE IF NOT EXISTS `roles` (
+    `role_id` int NOT NULL AUTO_INCREMENT,
+    `role_name` varchar(50) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` TIMESTAMP DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`role_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `address` (
+    `address_id` int NOT NULL AUTO_INCREMENT,
+    `address1` varchar(200) NOT NULL,
+    `address2` varchar(200) DEFAULT NULL,
+    `city` varchar(50) NOT NULL,
+    `state` varchar(50) NOT NULL,
+    `zip_code` int NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` TIMESTAMP DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`address_id`)
+);
+
 CREATE TABLE IF NOT EXISTS `user` (
     `user_id` int AUTO_INCREMENT NOT NULL,
     `name` varchar(50) NOT NULL,
     `email` varchar(50) NOT NULL,
     `password` varchar(12) NOT NULL,
     `mobile_num` varchar(10) NOT NULL,
-    PRIMARY KEY (`user_id`)
+    `role_id` int NOT NULL,
+    `address_id` int NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `created_by` varchar(50) NOT NULL,
+    `updated_at` TIMESTAMP DEFAULT NULL,
+    `updated_by` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`user_id`),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    FOREIGN KEY (address_id) REFERENCES address(address_id)
 );
 
 CREATE TABLE IF NOT EXISTS `order` (
